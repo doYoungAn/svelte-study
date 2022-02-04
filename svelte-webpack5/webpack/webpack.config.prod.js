b1e7f12d1133f5d051db6e2a62da3860ddbb5c58
안도영 <doyoung@marketboro.com>
2022-02-04 20:14:02 +0900

    webpack5를 이용하여 스벨트 기초적인 세팅 작업

---
+++ svelte-webpack5/webpack/webpack.config.prod.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CONFIG = {
+    mode: 'production',
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(svelte)$/,
+                use: {
+                    loader: 'svelte-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            svelte: path.dirname(require.resolve('svelte/package.json')),
+            extensions: ['.js', '.svelte'],
+            mainFields: ['svelte', 'browser', 'module', 'main'],
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './../public/index.html'),
+        }),
+    ],
+}
+
+module.exports = CONFIG;
