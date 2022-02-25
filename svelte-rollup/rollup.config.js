8ba520c7b3230b1aabf9c2abb65d3a65b43dea6a
doYoungAn <jispoop@gmail.com>
2022-02-25 23:29:57 +0900

    rollup svelte 프로젝트에 sass 적용

--- svelte-rollup/rollup.config.js
+++ svelte-rollup/rollup.config.js
@@ -1,8 +1,9 @@
 const svelte = require('rollup-plugin-svelte');
-const typescript = require('@rollup/plugin-typescript')
-const serve = require('rollup-plugin-serve')
-const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const typescript = require('@rollup/plugin-typescript');
+const serve = require('rollup-plugin-serve');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const livereload = require('rollup-plugin-livereload');
+const scss = require('rollup-plugin-scss');
 
 const NODE_ENV = process.env.NODE_ENV;
 console.log('NODE_ENV', NODE_ENV);
@@ -18,6 +19,7 @@ const CONFIG = {
             include: './src/**/*.svelte'
         }),
         typescript(),
+        scss(),
         NODE_ENV === 'dev' ? serve('public') : null,
         NODE_ENV === 'dev' ? livereload({ watch: 'public' }) : null,
     ]
